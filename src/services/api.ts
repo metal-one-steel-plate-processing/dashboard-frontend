380986c7f22d7ba981397c16c8dc8ec4a512722f
Ednaldo Ferreira <naldo_a.f@hotmail.com>
2021-10-27 09:04:16 -0300

    alterando api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://3.20.238.219:3333',
+  // baseURL: 'http://3.20.238.219:3333',
 
   // baseURL: 'http://localhost:3333',
+
+  baseURL: 'https://apidashboard.mosb.com.br',
 });
 
 export default api;
